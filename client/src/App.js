cc8d5bcaa82e0e1a0df012640c2d4f2a7fe1be1f
Kelvin Almonte <kelvin.almonte95@gmail.com>
2020-06-10 18:39:34 -0400

    Protected routes that requires authentication.

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,29 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
+import Home from './pages/Home';
+import Explore from './pages/Explore';
+import Profile from './pages/Profile';
+import Notifications from './pages/Notifications';
+import Messenger from './pages/Messenger';
+import PrivateRoute from './hoc/RoutesProtection/PrivateRoute';
+
 function App() {
   return (
-    <div className="App">
-      <h1>Hello World!</h1>
-    </div>
+    <React.Fragment>
+      <Router>
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <PrivateRoute exact path="/explore" component={Explore} />
+          <PrivateRoute exact path="/notifications" component={Notifications} />
+          <PrivateRoute exact path="/messages" component={Messenger} />
+          <PrivateRoute exact path="/profile" component={Profile} />
+        </Switch>
+      </Router>
+    </React.Fragment>
   );
 }
 
 export default App;
+
+{/* <Route exact path="/profile" component={Profile} /> */ }
