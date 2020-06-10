5ea3573162caabb0756980d7a86cdd02fa8c2a76
Kelvin Almonte <kelvin.almonte95@gmail.com>
2020-06-10 18:41:39 -0400

    Modified auth.js for user login

--- client/src/utils/auth.js
+++ client/src/utils/auth.js
@@ -1,9 +1,13 @@
 import axios from 'axios';
 
 export default {
-  // Authenticate if an user exist
+  // Authenticate if an user exist or sign in.
   authenticate: (data) => axios.post("/api/auth/login", data),
 
   // Create a new user
-  regrister: (query) => axios.post("/api/auth/login", query)
+  regrister: (query) => axios.post("/api/auth/login", query),
+
+  // This checks if there is a token saved to localstorage.
+  // If there is no data saved to localstorage then routes are protected.
+  isLogin: () => localStorage.getItem("token") ? true : false
 }
